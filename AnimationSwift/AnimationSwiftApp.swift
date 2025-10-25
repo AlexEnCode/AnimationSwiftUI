d2a71a643c10e68b0f4cb1b66de75c80cccd32ad
AlexEnCode <alexgabrielline@gmail.com>
2025-10-25 14:30:25 +0200

    Initial Commit

---
+++ AnimationSwift/AnimationSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftApp.swift
+//  AnimationSwift
+//
+//  Created by apprenant74 on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
